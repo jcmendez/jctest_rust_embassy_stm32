416a531c87f1493b279d9a967d0c3efe8ba6735a
Juan C. Mendez <jcmendez@alum.mit.edu>
2024-11-15 12:15:58 -0500

    Added doc and changed logic a bit

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # jctest_rust_embassy_stm32
 
 This is a simple exercise for me to learn developing firmware
-for STM microcontrollers using rust and embassy
+for STM microcontrollers using rust and [embassy](https://embassy.dev)
 
 ## Setup
+
+Install the [rustup](https://rustup.rs) toolchain installer, to compile Rust code.
+Install [probe-rs](https://probe.rs/) - to flash the firmware on your device. If you already have other tools like OpenOCD setup, you can use that as well.
 
 ## Run
 
@@ -11,3 +14,5 @@ for STM microcontrollers using rust and embassy
 cargo build --bin jctest
 ```
 
+
+This skeleton app was created using [cargo-embassy](https://github.com/adinack/cargo-embassy)
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use core::fmt::Write;
 use embassy_executor::Spawner;
 use embassy_stm32::{rcc::{Pll, PllMul, PllPreDiv, PllQDiv, PllRDiv, PllSource}, usart::UartTx};
 use embassy_stm32::rng::Rng;
-use embassy_stm32::usart::{Config as UsartConfig};
+use embassy_stm32::usart::Config as UsartConfig;
 use embassy_stm32::{bind_interrupts, peripherals, rng, Config};
 use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_time::{Duration, Timer};
@@ -37,9 +37,9 @@ async fn main(_spawner: Spawner) {
         divr: Some(PllRDiv::DIV4), // sysclk 72Mhz clock (16 / 1 * 18 / 4)
     });
     let p = embassy_stm32::init(config);
-    let config = UsartConfig::default();
+    let usart_config = UsartConfig::default();
     // PB11, PC1 or PA2 could be used as TX pin for LPUART1
-    let mut usart = UartTx::new(p.LPUART1, p.PC1, p.DMA1_CH1, config).unwrap();
+    let mut usart = UartTx::new(p.LPUART1, p.PC1, p.DMA1_CH1, usart_config).unwrap();
 
     info!("Hello, World!");
     let button = Input::new(p.PC13, Pull::Up);
@@ -49,19 +49,22 @@ async fn main(_spawner: Spawner) {
 
 
     loop {
-        unwrap!(rng.async_fill_bytes(&mut buf).await);
-        info!("random bytes: {:02x}", buf); 
-        let mut s: String<128> = String::new();
-        core::write!(&mut s, "Hello random {:02x?}!\r\n", buf).unwrap();
-   
         if button.is_high() {
             led.set_high();
             info!("Button is pressed");
+            let start = embassy_time::Instant::now();
+            while embassy_time::Instant::now() - start < Duration::from_secs(15) {
+                unwrap!(rng.async_fill_bytes(&mut buf).await);
+                info!("random bytes: {:02x}", buf); 
+                let mut s: String<128> = String::new();
+                core::write!(&mut s, "Random bytes: {:02x?}\r\n", buf).unwrap();
+                unwrap!(usart.blocking_write(s.as_bytes()));
+                Timer::after(Duration::from_millis(100)).await;
+            }
         } else {
             led.set_low();
             info!("Button is not pressed");
         }
-        unwrap!(usart.blocking_write(s.as_bytes()));
         Timer::after(Duration::from_millis(50)).await;
     }
 }
