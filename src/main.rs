a39b6b7f3fad75799105fd7aae8ac831e90c9ed3
Juan C. Mendez <jcmendez@alum.mit.edu>
2024-11-16 18:42:37 -0500

    Add USART3 support and refactor LED blinking logic

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ use defmt::*;
 use core::fmt::Write;
 use core::sync::atomic::{AtomicU32, Ordering};
 use embassy_executor::Spawner;
-use embassy_stm32::{exti::ExtiInput, rcc::{Pll, PllMul, PllPreDiv, PllQDiv, PllRDiv, PllSource}, usart::UartTx};
-use embassy_stm32::rng::Rng;
+use embassy_stm32::{exti::ExtiInput, gpio::OutputOpenDrain, rcc::{Pll, PllMul, PllPreDiv, PllQDiv, PllRDiv, PllSource}};
 use embassy_stm32::usart::{Config as UsartConfig, Uart};
 use embassy_stm32::{usart, bind_interrupts, peripherals, rng, Config};
 use embassy_stm32::gpio::{Pin, AnyPin, Input, Level, Output, Pull, Speed};
@@ -32,6 +31,7 @@ static BUFFER_UPDATED: Signal<ThreadModeRawMutex, ()> = Signal::new();
 bind_interrupts!(struct Irqs {
     RNG => rng::InterruptHandler<peripherals::RNG>;
     USART2 => usart::InterruptHandler<peripherals::USART2>;
+    USART3 => usart::InterruptHandler<peripherals::USART3>;
 });
 
 #[embassy_executor::task]
@@ -44,6 +44,14 @@ async fn blue_led_task(led: AnyPin) {
         Timer::after(Duration::from_millis(del.into())).await;
         led.toggle();
     }
+}
+
+fn set_fast_blink() {
+    BLINK_MS.store(100, Ordering::Relaxed);
+}
+
+fn set_slow_blink() {
+    BLINK_MS.store(1000, Ordering::Relaxed);
 }
 
 #[embassy_executor::task]
@@ -62,7 +70,6 @@ async fn red_led_task(led: AnyPin) {
     }
 }
 
-
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let mut config = Config::default();
@@ -76,70 +83,50 @@ async fn main(spawner: Spawner) {
         divr: Some(PllRDiv::DIV4), // sysclk 72Mhz clock (16 / 1 * 18 / 4)
     });
     let p = embassy_stm32::init(config);
-    let usart_config = UsartConfig::default();
-    let mut usart = UartTx::new(p.LPUART1, p.PC1, p.DMA1_CH1, usart_config).unwrap();
-    let mut usart2 = Uart::new(p.USART2, p.PD6, p.PD5, Irqs, p.DMA1_CH3, p.DMA1_CH4, UsartConfig::default()).unwrap();
 
-    info!("Hello, World!");
+    // let usart_config = UsartConfig::default();
+    // let mut usart = UartTx::new(p.LPUART1, p.PC1, p.DMA1_CH1, usart_config).unwrap();
+    // let mut usart2 = Uart::new(p.USART2, p.PD6, p.PD5, Irqs, p.DMA1_CH3, p.DMA1_CH4, UsartConfig::default()).unwrap();
+    let mut usart3 = Uart::new(p.USART3, p.PD9, p.PD8, Irqs, p.DMA1_CH2, p.DMA1_CH5, UsartConfig::default()).unwrap();
+    let mut sara_reset = OutputOpenDrain::new(p.PD10, Level::High, Speed::Low, Pull::Up);
+    let mut sara_power = OutputOpenDrain::new(p.PD13, Level::High, Speed::Low, Pull::None);
+
     let mut button = ExtiInput::new(Input::new(p.PC13, Pull::None), p.EXTI13);
-    
     let mut green_led = Output::new(p.PF6, Level::High, Speed::Low);
     //let mut blue_led = Output::new(p.PF7, Level::High, Speed::Low);
-    // let mut red_led = Output::new(p.PF8, Level::High, Speed::Low);
-    let mut rng = Rng::new(p.RNG, Irqs);
+    // let mut red_led = Output::new(p.PF8, Level::High, Speed::Low);    
+    info!("Hello, World!");
 
-    // Create and initialize a delay variable to manage delay loop
-    let mut del_var = 1000;
-
-    // Publish blink duration value to global context
-    BLINK_MS.store(del_var, Ordering::Relaxed);
- 
-     // Spawn LED blinking tasks
+    // Spawn LED blinking tasks
     spawner.spawn(blue_led_task(p.PF7.degrade())).unwrap();
     spawner.spawn(red_led_task(p.PF8.degrade())).unwrap();
 
+    set_slow_blink();
     green_led.set_low();
+    sara_power.set_low();
+    Timer::after(Duration::from_millis(2200)).await;
+    sara_power.set_high();
+    Timer::after(Duration::from_millis(400)).await;
+    sara_reset.set_low();
+    Timer::after(Duration::from_millis(200)).await;
+    sara_reset.set_high();
+    info!("SARA is powered on and reset");
+    Timer::after(Duration::from_millis(200)).await;
+
+    usart3.write("AT&F0\r\n".as_bytes()).await.ok();
+    info!("SARA is reset to factory settings");
+    usart3.write("ATE1\r\n".as_bytes()).await.ok();
+    info!("Echo is enabled");
+    usart3.write("AT+GMM\r\n".as_bytes()).await.ok();
+    info!("SARA firmware version is checked");
+    // Enable GPS
+    // usart3.write("AT+UGPS=1,0,1\r\n".as_bytes()).await.ok();
+    // info!("GPS is enabled");
+    // usart3.write("AT+CIND?\r\n".as_bytes()).await.ok();
+    green_led.set_high();
+    set_fast_blink();
+
     loop {
-        button.wait_for_rising_edge().await;
-        info!("Button is pressed");
-
-        // When button is pressed, turn on green LED and generate random bytes
-        green_led.set_high();
-        button.wait_for_falling_edge().await;
-        let mut local_buf = [0u8; 16];
-        unwrap!(rng.async_fill_bytes(&mut local_buf).await);
-        info!("random bytes: {:02x}", local_buf);
-
-        // Copy the random bytes to global buffer and signal the event
-        let mut buf = BUFFER_RNG.lock().await;
-        buf.copy_from_slice(&local_buf);
-        BUFFER_UPDATED.signal(());
-
-        // Print the random bytes to USART, finally turn off the green LED
-        let mut s: String<128> = String::new();
-        core::write!(&mut s, "Random bytes: {:02x?}\r\n", buf).unwrap();
-        unwrap!(usart.blocking_write(s.as_bytes()));
-        green_led.set_low();
-        
-        // Calculate the average of the random values
-
-        let mut sum: u32 = 0;
-        for &val in buf.iter() {
-            sum += val as u32;
-        }
-        let avg = sum / 16;
-
-        // If button pressed decrease the delay value
-        del_var = avg * 10;
-        info!("Delay value: {}", del_var);
-        // If updated delay value is below 100 then reset it back to starting value
-        if del_var <= 100 {
-            info!("Resetting delay value to 1000");
-            del_var = 1000;
-        }
-        // Publish updated delay value to global context
-        BLINK_MS.store(del_var, Ordering::Relaxed);
-        
-        usart2.write("ATZ\n".as_bytes()).await.ok();
+        button.wait_for_high().await;
     }
 }
